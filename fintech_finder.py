ba19cc945126e8eb3214d3c3014d4d50d19d8259
rosalinda <rolvera98271@gmail.com>
2023-08-06 19:37:04 -0700

    Made changes to py files

--- fintech_finder.py
+++ fintech_finder.py
@@ -30,6 +30,7 @@ from dataclasses import dataclass
 from typing import Any, List
 from web3 import Web3
 w3 = Web3(Web3.HTTPProvider('HTTP://127.0.0.1:7545'))
+
 ################################################################################
 # Step 1:
 # Import Ethereum Transaction Functions into the Fintech Finder Application
